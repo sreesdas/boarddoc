0abb41e745c9396cbea5e95fa0a5a654f4e808b9
sreesdas <sreenathsdas@gmail.com>
2018-02-02 09:28:45 +0530

    Added Material Sidebar

--- src/pages/committee/committee.ts
+++ src/pages/committee/committee.ts
@@ -15,13 +15,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CommitteePage {
 
-  committees = ['ONGC-Executive Commi Meeting','ONGC-Board Meeting','ONGC-Executive Meeting', 'ONGC-Testing Meeting', 'ONGC-corporate infocom Meeting'];
+//  committees = ['ONGC-Executive Commi Meeting','ONGC-Board Meeting','ONGC-Executive Meeting', 'ONGC-Testing Meeting', 'ONGC-corporate infocom Meeting'];
+
+  committees = [{ name: 'Committee1', hidden: true, description: 'Something'},
+  				{ name: 'Committee2', hidden: true, description: 'Something'},
+  				{ name: 'Committee3', hidden: true, description: 'Something'},
+  				{ name: 'Committee4', hidden: true, description: 'Something'}
+  				]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CommitteePage');
+  }
+
+  buttonClicked(selectedIndex){
+  	this.committees[selectedIndex].hidden = !this.committees[selectedIndex].hidden;
   }
 
 }
